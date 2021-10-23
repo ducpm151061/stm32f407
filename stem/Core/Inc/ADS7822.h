4fb1ddaf18782007c6a14dd0874e9ff38ff22ac3
ducpm151061 <duc.pm151061@sis.hust.edu.vn>
2021-10-23 20:50:28 +0700

    Delete stem directory

--- stem/Core/Inc/ADS7822.h
+++
@@ -1,9 +0,0 @@
-#ifndef __ADS7822_H__
-#define __ADS7822_H__
-
-#include "DataDefine.h"
-
-void ADS7822_Init(void);
-uint16_t ADS7822_GetOneSample(void);
-
-#endif
--- stem/Stem/Config/LCDConf.h
+++
@@ -1,8 +0,0 @@
-#ifndef __LCDCONF_H__
-#define __LCDCONF_H__
-
-#include "LCDConf.h"
-#include "DataDefine.h"
-
-
-#endif
